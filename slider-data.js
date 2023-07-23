57559101764cf6e8a829248bb07a5585d325dce0
Harshit Raj <131958146+itsharshitraj@users.noreply.github.com>
2023-07-23 13:47:51 +0530

    Disney + plus hotstar clone

---
+++ slider-data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'WI  vs IND 317/8 (50)',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'https://images.indianexpress.com/2019/08/ind-vs-wi-livestreaming-01.jpg'
+    },
+    {
+        name: 'Rudra: The Edge of Darkness',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/2180/1122180-h-0046ac044aa6'
+    },
+    {
+        name: 'The Book of Boba Fett',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'https://img1.hotstarext.com/image/upload/f_auto,t_hcdl/sources/r1/cms/prod/5318/1095318-h-a89162a0c1a9'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'https://img1.hotstarext.com/image/upload/f_auto,t_hcdl/sources/r1/cms/prod/7089/1037089-h-af0efbe1e4a7'
+    },
+    {
+        name: 'luca',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+        image: 'https://static.wixstatic.com/media/82f269_11911e14f81240828d83dd7ad62d90be~mv2.png/v1/fill/w_640,h_310,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/82f269_11911e14f81240828d83dd7ad62d90be~mv2.png'
+    }
+]
